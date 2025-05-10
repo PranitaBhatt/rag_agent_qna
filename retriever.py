d5babaa00c96ba44cc71d288ed0428858fc5799f
PranitaBhatt <pranitabhatt00@gmail.com>
2025-05-10 12:58:31 +0530

    retriver.py and faiss

--- retriever.py
+++ retriever.py
@@ -1,11 +1,12 @@
+import os
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.schema import Document
 
 def retrieve(query):
     try:
-        # Load local HuggingFace embeddings model
-        embeddings = HuggingFaceEmbeddings(model_name="./models/paraphrase-MiniLM-L3-v2")
+        model_path = os.path.abspath("./models/paraphrase-MiniLM-L3-v2")
+        embeddings = HuggingFaceEmbeddings(model_name=model_path)
 
         # Load FAISS vector store
         db = FAISS.load_local("vector_store", embeddings, allow_dangerous_deserialization=True)
